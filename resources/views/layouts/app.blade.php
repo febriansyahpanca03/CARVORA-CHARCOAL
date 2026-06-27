c19617675b004521a238bad2cbc872ebdd192087
febriansyahpanca03 <febriansyahpanca03@gmail.com>
2026-06-27 16:17:43 +0700

    Add favicon using Carvora logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,11 @@
     <title>@yield('title', 'Carvora Charcoal Indonesia – Premium Coconut Shell Charcoal')</title>
     <meta name="description" content="@yield('description', 'Produsen briket arang tempurung kelapa premium Indonesia. 100% alami, bebas kimia, ekspor ke 30+ negara.')">
 
+    {{-- Favicon --}}
+    <link rel="icon" type="image/png" href="/logo.png">
+    <link rel="shortcut icon" type="image/png" href="/logo.png">
+    <link rel="apple-touch-icon" href="/logo.png">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700;800;900&family=Inter:wght@300;400;500;600;700&family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet">
